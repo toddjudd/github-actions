7971478800c3c0552e91aac80cb8fdb435a42c9e
toddjudd <todd.judd@gmail.com>
2022-01-25 19:53:49 -0700

    Looks like jq is already installed trying to run node

---
+++ .github/workflows/test.js
@@ -0,0 +1,13 @@
+const { existsSync } = require('fs');
+const path = require('path');
+
+const event = process.env.GITHUB_EVENT_PATH
+  ? require(process.env.GITHUB_EVENT_PATH)
+  : {};
+
+if (!event.commits) {
+  console.log(
+    "Couldn't find any commits in this event, incrementing patch version..."
+  );
+}
+// (async () => {})();
