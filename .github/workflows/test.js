aa71e3b194d83efff79f3557b4ed5c6e1abe4da8
toddjudd <todd.judd@gmail.com>
2022-01-25 20:13:43 -0700

    here goes nothing using node to return string fro env

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,13 +1,14 @@
 const { existsSync } = require('fs');
 const path = require('path');
 
-const event = process.env.GITHUB_EVENT_PATH
-  ? require(process.env.GITHUB_EVENT_PATH)
-  : {};
+// const event = process.env.GITHUB_EVENT_PATH
+//   ? require(process.env.GITHUB_EVENT_PATH)
+//   : {};
 
-if (!event.commits) {
-  console.log(
-    "Couldn't find any commits in this event, incrementing patch version..."
-  );
-}
+// if (!event.commits) {
+//   console.log(
+//     "Couldn't find any commits in this event, incrementing patch version..."
+//   );
+// }
 // (async () => {})();
+process.stdout.write('major');
