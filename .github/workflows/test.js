fd29d9c3f4a82b2f35e3ae6d72e95ff2680d5609
toddjudd <todd.judd@gmail.com>
2022-01-25 20:34:35 -0700

    feat: trying to make the version say minor

--- .github/workflows/test.js
+++ .github/workflows/test.js
@@ -1,17 +1,30 @@
 const { existsSync } = require('fs');
 const path = require('path');
+const commitTypes = require('./commit-types');
+const versionTypes = ['patch', 'minor', 'major'];
 
-// const event = process.env.GITHUB_EVENT_PATH
-//   ? require(process.env.GITHUB_EVENT_PATH)
-//   : {};
+const event = process.env.GITHUB_EVENT_PATH
+  ? require(process.env.GITHUB_EVENT_PATH)
+  : {};
 
-// if (!event.commits) {
-//   console.log(
-//     "Couldn't find any commits in this event, incrementing patch version..."
-//   );
-// }
-// (async () => {})();
-process.stdout.write('major');
-console.log('minor');
-console.log('okay what about this');
-process.stdout.write('and this');
+if (!event.commits) {
+  console.log(
+    "Couldn't find any commits in this event, incrementing patch version..."
+  );
+}
+
+const newVersion =
+  versionTypes[
+    Math.max(
+      ...event.commits.map((commit) =>
+        versionTypes.findIndex(
+          (vt) =>
+            vt ==
+            commitTypes.find((t) => t.type === commit.split(/\(\:/)[0])
+              .newversionType
+        )
+      )
+    )
+  ];
+
+process.stdout.write(newVersion || 'patch');
